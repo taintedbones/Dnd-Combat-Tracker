51311a83c6495d5a5147d1c6198391de7a3e9013
kierstyn <kjust@csu.fullerton.edu>
2020-06-06 21:54:11 -0700

    Organized functions by page

--- Dnd-Combat-Tracker/mainwindow.h
+++ Dnd-Combat-Tracker/mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include "Database.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -21,17 +21,15 @@ public:
 private slots:
     //************ Welcome Page *********************
     void on_welcomeStart_pushButton_clicked();
-        void on_dbOpt_welcome_pushButton_clicked();
+    void on_dbOpt_welcome_pushButton_clicked();
 
     //************ Edit Page *********************
     void on_back_editPage_pushButton_clicked();
-
     void on_next_editPage_pushButton_clicked();
-       void on_dbOpt_editPage_pushButton_clicked();
+    void on_dbOpt_editPage_pushButton_clicked();
 
     //************ Assign Page *********************
     void on_back_assignInit_pushButton_clicked();
-
     void on_fight_assignInit_pushButton_clicked();
 
     //************ Combat Page *********************
@@ -39,10 +37,10 @@ private slots:
 
     //************ DB Edit Page *********************
     void on_mainMenu_pushButton_clicked();
-
     void on_combatEditor_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+    Database *db;
 };
 #endif // MAINWINDOW_H
