e6929895e0a972dd8c30fb1b416cdc5e9db556dd
kierstyn <kjust@csu.fullerton.edu>
2020-06-06 21:54:37 -0700

    Added comments to functions

--- Dnd-Combat-Tracker/mainwindow.cpp
+++ Dnd-Combat-Tracker/mainwindow.cpp
@@ -8,6 +8,9 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     ui->main_stackedWidget->setCurrentIndex(WELCOME);
+
+    db = new Database("../itdb.db", "QSQLITE");
+
 }
 
 MainWindow::~MainWindow()
@@ -15,51 +18,61 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+// Navigates user to combat edit page from welcome page
 void MainWindow::on_welcomeStart_pushButton_clicked()
 {
     ui->main_stackedWidget->setCurrentIndex(EDIT);
 }
 
+// Navigates user to welcome page from combat edit page
 void MainWindow::on_back_editPage_pushButton_clicked()
 {
     ui->main_stackedWidget->setCurrentIndex(WELCOME);
 }
 
+// Navigates user to assign initiative page from combat edit page
 void MainWindow::on_next_editPage_pushButton_clicked()
 {
     ui->main_stackedWidget->setCurrentIndex(ASSIGN);
 }
 
+// Navigates user to combat edit page from assign initiative page
 void MainWindow::on_back_assignInit_pushButton_clicked()
 {
     ui->main_stackedWidget->setCurrentIndex(EDIT);
 }
 
+// Navigates user to combat page from assign initiative page
 void MainWindow::on_fight_assignInit_pushButton_clicked()
 {
     ui->main_stackedWidget->setCurrentIndex(COMBAT);
 }
 
+// Navigates user to welcome page from combat page
 void MainWindow::on_endCombat_pushButton_clicked()
 {
     ui->main_stackedWidget->setCurrentIndex(WELCOME);
 }
 
+// Navigates user to database editor page from welcome page
 void MainWindow::on_dbOpt_welcome_pushButton_clicked()
 {
     ui->main_stackedWidget->setCurrentIndex(DB_EDIT);
 }
 
+// Navigates user to database editor page from combat edit page
 void MainWindow::on_dbOpt_editPage_pushButton_clicked()
 {
     ui->main_stackedWidget->setCurrentIndex(DB_EDIT);
 }
 
+// Navigates user to welcome page from database editor page
 void MainWindow::on_mainMenu_pushButton_clicked()
 {
     ui->main_stackedWidget->setCurrentIndex(WELCOME);
 }
 
+// Navigates user to combat edit page from database editor page
 void MainWindow::on_combatEditor_pushButton_clicked()
 {
     ui->main_stackedWidget->setCurrentIndex(EDIT);
