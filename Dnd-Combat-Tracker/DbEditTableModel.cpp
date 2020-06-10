62756682659d577f68b87d9821cb2b1c933ad843
kierstyn <kjust@csu.fullerton.edu>
2020-06-09 19:30:50 -0700

    Created table model for db edit table view

---
+++ Dnd-Combat-Tracker/DbEditTableModel.cpp
@@ -0,0 +1,6 @@
+#include "DbEditTableModel.h"
+
+DbEditTableModel::DbEditTableModel()
+{
+
+}
---
+++ Dnd-Combat-Tracker/DbEditTableModel.h
@@ -0,0 +1,11 @@
+#ifndef DBEDITTABLEMODEL_H
+#define DBEDITTABLEMODEL_H
+
+
+class DbEditTableModel
+{
+public:
+    DbEditTableModel();
+};
+
+#endif // DBEDITTABLEMODEL_H
