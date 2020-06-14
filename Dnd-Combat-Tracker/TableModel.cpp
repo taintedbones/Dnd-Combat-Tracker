f05077530c80ae7fcc0eaf4bf500e5f82a465093
Weston Mathews <westonmathews@gmail.com>
2020-06-14 13:10:40 -0700

    Changed params to pointers

--- Dnd-Combat-Tracker/TableModel.cpp
+++ Dnd-Combat-Tracker/TableModel.cpp
@@ -1,27 +1,27 @@
 #include "TableModel.h"
 
 // Model used for 'combat_page' (Conduct Combat) Page
-void TableModel::InitializeCombatModel(QTableWidget &combatTable)
+void TableModel::InitializeCombatModel(QTableWidget *combatTable)
 {
-    combatTable.clearContents();
-    combatTable.setColumnCount(CombatColCount);
-    combatTable.setVerticalHeaderLabels(CombatColNames);
+    combatTable->clearContents();
+    combatTable->setColumnCount(CombatColCount);
+    combatTable->setVerticalHeaderLabels(CombatColNames);
 }
 
 // Model used for 'edit_page' (Add Actors) Page
-void TableModel::InitializeActorListModel(QTableWidget &addActors)
+void TableModel::InitializeActorListModel(QTableWidget *addActors)
 {
-    addActors.clearContents();
-    addActors.setColumnCount(ActorListColCount);
-    addActors.setVerticalHeaderLabels(ActorListColNames);
+    addActors->clearContents();
+    addActors->setColumnCount(ActorListColCount);
+    addActors->setVerticalHeaderLabels(ActorListColNames);
 }
 
 // Model used for 'assignInit_page' (Assign Initiative) Page
-void TableModel::InitializeInitiativeModel(QTableWidget &assignInit)
+void TableModel::InitializeInitiativeModel(QTableWidget *assignInit)
 {
-    assignInit.clearContents();
-    assignInit.setColumnCount(AssignInitColCount);
-    assignInit.setVerticalHeaderLabels(AssignInitColNames);
+    assignInit->clearContents();
+    assignInit->setColumnCount(AssignInitColCount);
+    assignInit->setVerticalHeaderLabels(AssignInitColNames);
 }
 
 // Constructor
